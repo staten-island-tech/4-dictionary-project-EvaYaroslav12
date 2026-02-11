e9a4a1ccd9db0b005e516f0aca3d220d402d5f6a
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-02-11 09:23:55 -0500

    .

---
+++ dictiony.py
@@ -0,0 +1,9 @@
+
+item = {
+    "name": "Samsung 55\" 4K UHD TV",
+    "price": 429.99,
+    "department": "Televisions",
+    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+}
+
+print (item["name"],item['price'])
